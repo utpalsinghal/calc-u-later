82ad50b3b08cd54ccfd74db444415e50c0dbbcc2
Utpal Singhal <utpal.singhal499@gmail.com>
2023-07-29 20:28:08 +0530

    feat: handle onClick events - add validation expression validations - add delete key

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,26 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { evaluate, round } from "mathjs";
 
 const Calculator = () => {
+	const [expression, setExpression] = useState("");
+
 	const handleOperand = (e) => {
-		alert(e.target.innerText);
-		console.log("ehandleOperand", e);
+		const val = e.target.innerText;
+		let tempExpression = expression + val;
+		setExpression(tempExpression);
 	};
-	const handleReset = (e) => {
-		alert(e.target.innerText);
-		console.log("ehandleReset", e);
-	};
+
 	const handleOperator = (e) => {
-		alert(e.target.innerText);
-		console.log("ehandleOperator", e);
+		const val = e.target.innerText;
+		const inputValueLen = expression.length;
+		const lastInput = expression.charAt(inputValueLen - 1);
+		let tempExpression = "";
+		if (
+			lastInput === "÷" ||
+			lastInput === "x" ||
+			lastInput === "-" ||
+			lastInput === "+"
+		) {
+			tempExpression = expression.substring(0, inputValueLen - 1) + val;
+		} else {
+			tempExpression = expression + val;
+		}
+		setExpression(tempExpression);
 	};
+
+	const handleDelete = (e) => {
+		const inputValueLen = expression.length;
+		let tempExpression = expression.substring(0, inputValueLen - 1);
+		setExpression(tempExpression);
+	};
+
+	const handleReset = (e) => {
+		setExpression("");
+	};
+
 	const handleCalculate = (e) => {
-		alert(e.target.innerText);
-		console.log("ehandleCalculate", e);
+		let tempExpression = expression;
+		tempExpression = tempExpression.replace("÷", "/");
+		tempExpression = tempExpression.replace("x", "*");
+		tempExpression = tempExpression.length === 0 ? "0" : tempExpression;
+
+		let result;
+
+		try {
+			if (!areBracketsBalanced()) {
+				const error = { message: "Brackets are not balanced!", code: 1 };
+				throw error;
+			}
+			result = evaluate(tempExpression);
+		} catch (error) {
+			if (error.code === 1) {
+				alert(error.message);
+			} else {
+				alert("Invalid Input");
+			}
+			return;
+		}
+
+		result = round(result, 8).toString();
+		if (result === "0") {
+			setExpression("");
+		} else {
+			setExpression(result);
+		}
 	};
+
+	const areBracketsBalanced = () => {
+		let stack = [];
+		for (let i = 0; i < expression.length; i++) {
+			let element = expression.charAt(i);
+			if (element === "(") {
+				stack.push(element);
+				continue;
+			} else if (element === ")") {
+				if (stack.length === 0) return false;
+				stack.pop();
+			}
+		}
+		if (stack.length === 0) return true;
+		else return false;
+	};
+
 	return (
 		<div className='calculator'>
 			<div className='formula'>(0.00+0.00)</div>
-			<div className='result'>0.00</div>
+			<div className='result'>
+				<p>{expression === "" ? 0 : expression}</p>
+			</div>
 			<div className='keypad'>
 				<div className='row'>
 					<div className='col' onClick={handleOperand}>
@@ -32,7 +102,10 @@ const Calculator = () => {
 					<div className='col' onClick={handleOperator}>
 						<p>{`%`}</p>
 					</div>
-					<div className='col' onClick={handleReset}>
+					<div className='col half' onClick={handleDelete}>
+						<p>{`DEL`}</p>
+					</div>
+					<div className='col half' onClick={handleReset}>
 						<p>{`AC`}</p>
 					</div>
 				</div>
